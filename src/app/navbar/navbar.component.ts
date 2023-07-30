6c1f422978ba33da0cd5a2af66ab30e55401b9c9
Hamed <jabbary.hamed@gmail.com>
2023-07-29 17:49:03 -0700

    change

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-navbar',
+  template: `<h1> Navbar Template </h1>
+             <p> Test</p>
+   `,
+  styles: [`h1 {
+
+    color: blue;
+    font-size: 18px;
+  }`]
+})
+
+
+export class NavbarComponent {
+
+
+}
